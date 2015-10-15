5b79c183c3372f566d0c537efddffeeacc458529
AngelaDon <angeladon25@yahoo.com>
2015-10-14 22:46:43 -0400

    Initial Commit

---
+++ UITextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by Angela Don on 10/14/15.
+//  Copyright © 2015 Angela Don. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
